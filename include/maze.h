442c6acf3e1ec858a2173d3689d04c2d26b0e01c
rwkliu <114320485+rwkliu@users.noreply.github.com>
2024-05-11 10:37:50 -0700

    Change maze to double pointer

--- include/maze.h
+++ include/maze.h
@@ -4,10 +4,11 @@
 typedef struct s_maze {
   int row;
   int col;
-  char *maze;
+  char **maze;
 } maze_s;
 
 int get_integer(char *int_string, int start, int end);
 maze_s *get_maze_size(char *maze_size_string, maze_s *maze_struct);
+maze_s *initialize_maze(int fd);
 
 #endif
