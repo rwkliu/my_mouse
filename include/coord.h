1bf833df9c85ef2120aa9d11da6f62a93561fa75
rwkliu <114320485+rwkliu@users.noreply.github.com>
2024-05-13 23:34:27 -0700

    Add coordinate struct and setup and teardown functions

---
+++ include/coord.h
@@ -0,0 +1,12 @@
+#ifndef COORD_H
+#define COORD_H
+
+typedef struct {
+    int row;
+    int col;
+}   coord;
+
+coord *coord_new();
+void coord_free(coord *coord_s);
+
+#endif
---
+++ src/coord.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "coord.h"
+
+coord *coord_new() {
+    coord *new_coord = malloc(sizeof(coord));
+    new_coord->row = 0;
+    new_coord->col = 0;
+    return new_coord;
+}
+
+void coord_free(coord *coord_s) {
+    free(coord_s);
+}
