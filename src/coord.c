523968749642cd568d4696de9fe82318275d3d1a
rwkliu <114320485+rwkliu@users.noreply.github.com>
2024-05-14 17:28:54 -0700

    Add coord_free function

--- src/coord.c
+++ src/coord.c
@@ -10,5 +10,13 @@ coord *coord_new(int row, int col) {
 }
 
 void coord_free(coord *coord_s) {
+    coord *coord_ptr = coord_s;
+    coord *coord_to_delete = coord_s;
+
+    while (coord_ptr) {
+        coord_ptr = coord_ptr->next;
+        free(coord_to_delete);
+        coord_to_delete = coord_ptr;
+    }
     free(coord_s);
 }
