a9b1502ee6f45ebc85af739999fb590978ad9e71
rwkliu <114320485+rwkliu@users.noreply.github.com>
2024-05-14 22:27:19 -0700

    Change indentation to 4 and create and free coord_array

--- src/main.c
+++ src/main.c
@@ -4,20 +4,24 @@
 #include "file.h"
 #include "helpers.h"
 #include "maze.h"
+#include "coord_array.h"
 
 int main(int argc, char **argv) {
-  int fd = open_file(argv[1]);
-  if (fd == -1) {
-    printf("Error reading file\n");
-    return 1;
-  }
-  maze_s *maze_1 = initialize_maze(fd);
-  printf("rows: %d\n", maze_1->row);
-  printf("cols: %d\n", maze_1->col);
-  print_maze(maze_1);
-  printf("start row: %d, start col: %d\n", maze_1->start_row, maze_1->start_col);
-  printf("end row: %d, end col: %d\n", maze_1->end_row, maze_1->end_col);
+    int fd = open_file(argv[1]);
+    if (fd == -1) {
+        printf("Error reading file\n");
+        return 1;
+    }
+    maze_s *maze_1 = initialize_maze(fd);
+    printf("rows: %d\n", maze_1->row);
+    printf("cols: %d\n", maze_1->col);
+    print_maze(maze_1);
+    printf("start row: %d, start col: %d\n", maze_1->start_row, maze_1->start_col);
+    printf("end row: %d, end col: %d\n", maze_1->end_row, maze_1->end_col);
 
-  free_maze(maze_1);
-  return 0;
+    coord_array *visited = coord_array_new();
+    coord_array_free(visited);
+
+    free_maze(maze_1);
+    return 0;
 }
